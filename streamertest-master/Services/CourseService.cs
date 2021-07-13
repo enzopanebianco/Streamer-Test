01b491fcad73e1c64fae1a45678f0855bdbdd687
Enzo Romano Panebianco Enzo <enzinho.983@gmail.com>
2021-07-13 14:11:23 -0300

    delete gitignore

---
+++ streamertest-master/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SS_API.Model;
+using SS_API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SS_API.Services
+{
+    public class CourseService
+    {
+        private readonly StreamerContext _context;
+
+        public CourseService(StreamerContext context)
+        {
+            _context = context;
+        }
+//Lista de cursos usada para cadastrar os projetos
+        public List<Course> Index()
+        {
+            return _context.Courses.ToList();
+        }
+    }
+}
---
+++ streamertest-master/Services/ProjectService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SS_API.Model;
+using SS_API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SS_API.Services
+{
+    public class ProjectService
+    {
+        private readonly StreamerContext _context;
+
+        public ProjectService(StreamerContext context)
+        {
+            _context = context;
+        }
+
+        public List<Project> Index()
+        {
+                return _context.Projects.Include(x=>x.Course).ToList();
+        }
+
+        public void Create(Project project)
+        {
+                _context.Projects.Add(project);
+                _context.SaveChanges();   
+        }
+        public Project GetProjectById(int projectId)
+        {
+            return _context.Projects.Where(x=>x.Id==projectId).Include(x => x.Course).FirstOrDefault();
+        }
+        public List<Project> GetProjectByCourseId(int courseId)
+        {
+
+            List<Project> projects = _context.Projects.Where(x => x.CourseId == courseId).Include(x => x.Course).ToList();
+           
+            return projects;
+        }
+        public void UpdateProject(Project project)
+        {
+                
+                _context.Projects.Update(project);
+                _context.SaveChanges();
+        }
+
+        public void DeleteProject(int id)
+        {
+            Project project = _context.Projects.Find(id);
+
+            if (project != null)
+            {
+                _context.Projects.Remove(project);
+                _context.SaveChanges();
+            }
+
+        }
+    }
+}
